ed9d1fd77ba34685b6628bb812829575ab4ccfce
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-14 15:31:24 +0200

    Ex1 finsihed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -14,15 +18,28 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def show
-    @task = Task.find(params[:id])
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
